e99f040cd00affe1a4066c7b0f005a4940e431a3
MIcoder003 <72258775+MIcoder003@users.noreply.github.com>
2022-11-20 11:20:32 +0530

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,49 +1,59 @@
 const form = document.querySelector('#img-form');
 const img = document.querySelector('#img');
-const result = document.querySelector('#result');
+const outputPath = document.querySelector('#output-path');
 const filename = document.querySelector('#filename');
 
 
 function loadImage(e) {
-    const file = e.target.files[0];
+  const file = e.target.files[0];
 
-    if(!isFileImage(file)) {
-        console.log('Please select an image');
+  if (!isFileImage(file)) {
+      console.log('Please select an image file');
         return;
-    }
+  }
 
-    form.style.display = 'block';
-    filename.innerText = file.name;
+  form.style.display = 'block';
+  filename.innerText = file.name;
+
 }
 
-// Send image to main
-function sendImage(e){
-    e.preventDefault();
+// Send image to main process
+function detectDisease(e) {
+  e.preventDefault();
 
-    const imgPath = img.files[0].path;
+  const imgPath = img.files[0].path;
+  const imgName = img.files[0].name;
 
-    if(!img.files[0]) {
-        console.log('Please upload an image');
-        return;
-    }
+  if(!img.files[0]) {
+     console.log('Please upload an image');
 
-    
-   //Send to main with IPCrenderer
-   ipcRenderer.send('image:detect', {
-        imgPath
-   });
+  }
+
+  //Send to main using ipcRenderer
+  ipcRenderer.send('image:detect', {
+    imgPath,
+    imgName
+  });
 }
 
-/*Catch image done event
-ipcRenderer.on ('image:done', () => {
-    result.innerText = "Healthy"
-})*/
+//When done, show result
 
-// Check  file is image
+ipcRenderer.on('image:done', () => {
+  if (leaf_health == true) {
+    result.innerText = "Diseased";
+  }
+
+  if (leaf_health == false) {
+    result.innerText = "Healthy";
+  }
+});
+
+
+//Make Sure File is image 
 function isFileImage(file) {
-const acceptedImageTypes = ['image/png', 'image/jpeg'];
-return file && acceptedImageTypes.includes(file['type']);
+  const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+  return file && acceptedImageTypes.includes(file['type'])
 }
-img.addEventListener('change', loadImage);
-form.addEventListener('submit', sendImage);
 
+img.addEventListener('change', loadImage);
+form.addEventListener('submit', detectDisease);
