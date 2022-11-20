a45ab8741398be2a0652832e3b131d29a429430e
MIcoder003 <72258775+MIcoder003@users.noreply.github.com>
2022-11-20 11:19:10 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,5 @@
-const os = require('os')
-const path = require('path')
 const {contextBridge, ipcRenderer } = require('electron');
 
-Window.ipcRenderer;
 contextBridge.exposeInMainWorld('ipcRenderer', {
     send: (channel, data) => ipcRenderer.send(channel, data) ,
     on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)), 
